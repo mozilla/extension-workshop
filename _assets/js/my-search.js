883f611fe823f6a7df210211772749f9a6195f00
Lance Cummings <lance@glance.ca>
2019-06-04 17:00:24 -0700

    Ran prettier

--- _assets/js/my-search.js
+++ _assets/js/my-search.js
@@ -1,26 +1,24 @@
 'use strict';
 
 function renderJekyllPagesApiSearchResults(query, results, doc, resultsElem) {
-
   var list_title = doc.createElement('h2'),
-      list = doc.createElement('ol'),
-      num = 0;
+    list = doc.createElement('ol'),
+    num = 0;
 
-  list_title.classList.add("no-underline");
+  list_title.classList.add('no-underline');
 
   results.forEach(function(result, index) {
-
     if (!result.title) return true;
 
     var item = doc.createElement('li'),
-        link = doc.createElement('a'),
-        title = doc.createElement('h3'),
-        excerpt = doc.createTextNode('Excerpt to go here...'),
-        url = doc.createTextNode(location.origin + result.url),
-        body = doc.createElement('p'),
-        body_2 = doc.createElement('p'),
-        small = doc.createElement('small'),
-        text = doc.createTextNode(result.title);
+      link = doc.createElement('a'),
+      title = doc.createElement('h3'),
+      excerpt = doc.createTextNode('Excerpt to go here...'),
+      url = doc.createTextNode(location.origin + result.url),
+      body = doc.createElement('p'),
+      body_2 = doc.createElement('p'),
+      small = doc.createElement('small'),
+      text = doc.createTextNode(result.title);
 
     title.appendChild(text);
     small.appendChild(url);
@@ -42,7 +40,9 @@ function renderJekyllPagesApiSearchResults(query, results, doc, resultsElem) {
     num++;
   });
 
-  var list_title_text = doc.createTextNode(num + ' results for "' + query + '"');
+  var list_title_text = doc.createTextNode(
+    num + ' results for "' + query + '"'
+  );
   list_title.appendChild(list_title_text);
   resultsElem.appendChild(list_title);
   resultsElem.appendChild(list);
@@ -50,4 +50,4 @@ function renderJekyllPagesApiSearchResults(query, results, doc, resultsElem) {
 
 module.exports = function(query, results, doc, resultsList) {
   renderJekyllPagesApiSearchResults(query, results, doc, resultsList);
-}
+};
