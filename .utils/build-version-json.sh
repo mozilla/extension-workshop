7861b61c0204c0f68c4b6f71d3c92a6364129a50
Stuart Colville <scolville@mozilla.com>
2019-10-01 17:53:02 +0100

    Fix __version__ (#480)

--- .utils/build-version-json.sh
+++ .utils/build-version-json.sh
@@ -4,7 +4,7 @@ echo "$(dirname $0)"
 cd $(dirname $0)/..
 
 HASH=$(git --no-pager log --format=format:"%H" -1)
-TAG=$(git show-ref --tags | awk "/$HASH/ {print \$NF}" | sed 's/refs.tags.//')
+TAG=$(git describe --tags)
 
 printf '{"commit":"%s","version":"%s","source":"https://github.com/mozilla/extension-workshop"}\n' \
     "$HASH" \
