68109adb9287e12827c88852bdfd12649f78ae32
Stuart Colville <scolville@mozilla.com>
2020-07-03 19:06:52 +0100

    Get an initial homepage building

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,19 @@
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("_assets");
+
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: false,
+    strict_filters: true,
+    root: [
+      '_includes',
+      '.',
+    ],
+  });
+
   return {
     dir: {
-      input: "./",      // Equivalent to Jekyll's source property
+      input: "./index.md",      // Equivalent to Jekyll's source property
       output: "./_site", // Equivalent to Jekyll's destination property
       includes: "./_includes",
       layouts: "./_layouts"
