9f9168304b8a78a36a789dc1a09550b8ee5f1f89
Stuart Colville <scolville@mozilla.com>
2020-07-05 23:12:48 +0100

    Run prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,37 +1,33 @@
 const { Liquid } = require('liquidjs');
-const md = require("markdown-it")({
+const md = require('markdown-it')({
   html: true,
   breaks: true,
-  linkify: true
+  linkify: true,
 });
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const xmlFiltersPlugin = require('eleventy-xml-plugin')
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const xmlFiltersPlugin = require('eleventy-xml-plugin');
 const safeExternalLinks = require('eleventy-plugin-safe-external-links');
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   const liquidParser = new Liquid({
-    root: [
-      './src/_includes',
-      './src/_layouts'
-    ],
+    root: ['./src/_includes', './src/_layouts'],
     extname: '.liquid',
     dynamicPartials: false,
     strictFilters: true,
   });
 
-  eleventyConfig.setLibrary("liquid", liquidParser);
+  eleventyConfig.setLibrary('liquid', liquidParser);
 
   // Tell the config to not use gitignore for ignores.
   eleventyConfig.setUseGitIgnore(false);
 
   // Explicitly copy through the built files needed.
-  eleventyConfig.addPassthroughCopy("./src/_assets/img/");
-  eleventyConfig.addPassthroughCopy("./src/_assets/fonts/");
-  eleventyConfig.addPassthroughCopy("./src/_assets/js/basket-client.js");
-
+  eleventyConfig.addPassthroughCopy('./src/_assets/img/');
+  eleventyConfig.addPassthroughCopy('./src/_assets/fonts/');
+  eleventyConfig.addPassthroughCopy('./src/_assets/js/basket-client.js');
 
   // Plugins
-  eleventyConfig.addPlugin(xmlFiltersPlugin)
+  eleventyConfig.addPlugin(xmlFiltersPlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
   /*
   eleventyConfig.addPlugin(safeExternalLinks, {
@@ -45,18 +41,18 @@ module.exports = function(eleventyConfig) {
   */
 
   // Markdown instance in general plus for filter in templates.
-  eleventyConfig.setLibrary("md", md);
-  eleventyConfig.addFilter("markdownify", function(value) {
+  eleventyConfig.setLibrary('md', md);
+  eleventyConfig.addFilter('markdownify', function (value) {
     return md.render(value.toString());
   });
 
   return {
     templateFormats: ['md', 'liquid', 'html'],
     dir: {
-      input: "src",
-      output: "src/_site",
-      includes: "_includes",
-      layouts: "_layouts"
-    }
+      input: 'src',
+      output: 'src/_site',
+      includes: '_includes',
+      layouts: '_layouts',
+    },
   };
 };
