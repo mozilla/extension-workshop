a7da13c8067217bce9e296fd7623406b012d2a18
Stuart Colville <scolville@mozilla.com>
2020-08-07 21:22:17 +0100

    Run prettier

--- tests/assets-pipeline.test.js
+++ tests/assets-pipeline.test.js
@@ -8,7 +8,6 @@ const dest = path.resolve(__dirname, 'fixtures/dist/');
 let ap;
 
 describe('Asset Pipeline functions', () => {
-
   beforeEach(async () => {
     ap = new AssetPipeline(src, dest);
     await ap.recursiveListDir();
@@ -17,20 +16,27 @@ describe('Asset Pipeline functions', () => {
   describe('getContentHash', () => {
     it('should hash content', () => {
       const hash = ap.getContentHash('whatever');
-      expect(hash).toEqual('85738f8f9a7f1b04b5329c590ebcb9e425925c6d0984089c43a022de4f19c281');
+      expect(hash).toEqual(
+        '85738f8f9a7f1b04b5329c590ebcb9e425925c6d0984089c43a022de4f19c281'
+      );
     });
   });
 
   describe('getFileHash', () => {
     it('should hash a file', async () => {
-      const hash = await ap.getFileHash(path.join(__dirname, 'fixtures/hash.txt'));
-      expect(hash).toEqual('4fe366c97ef67271487fd682c7e4efa122338272c12df66003adfc3ec6c064af');
+      const hash = await ap.getFileHash(
+        path.join(__dirname, 'fixtures/hash.txt')
+      );
+      expect(hash).toEqual(
+        '4fe366c97ef67271487fd682c7e4efa122338272c12df66003adfc3ec6c064af'
+      );
     });
   });
 
   describe('getHashedPath', () => {
     it('should add a hash to a path', async () => {
-      const hash = '4fe366c97ef67271487fd682c7e4efa122338272c12df66003adfc3ec6c064af';
+      const hash =
+        '4fe366c97ef67271487fd682c7e4efa122338272c12df66003adfc3ec6c064af';
       const hashedPath = ap.getHashedPath('fixtures/hash.txt', hash);
       expect(hashedPath).toEqual('fixtures/hash.4fe366c9.txt');
     });
@@ -46,20 +52,20 @@ describe('Asset Pipeline functions', () => {
   describe('recursiveListDir', () => {
     it('should generate a map of files', async () => {
       expect(ap.assetMap).toEqual({
-        "assets/file1.css": {
-          "hashedPath": null,
+        'assets/file1.css': {
+          hashedPath: null,
         },
-        "assets/file2.js":  {
-          "hashedPath": null,
+        'assets/file2.js': {
+          hashedPath: null,
         },
-        "assets/octoamo-sm.png": {
-          "hashedPath": null,
+        'assets/octoamo-sm.png': {
+          hashedPath: null,
         },
-        "index.html": {
-          "hashedPath": null,
+        'index.html': {
+          hashedPath: null,
         },
-        "robots.txt": {
-          "hashedPath": null,
+        'robots.txt': {
+          hashedPath: null,
         },
       });
     });
@@ -88,28 +94,37 @@ describe('Asset Pipeline functions', () => {
     });
   });
 
-  describe('hasHashedReplacementURL',  () => {
+  describe('hasHashedReplacementURL', () => {
     const fileObj = {
       'foo/test.css': {
         hashedPath: 'foo/test.7ff8923a.css',
       },
       'foo/test2.css': {
-        hashedPath: null
+        hashedPath: null,
       },
     };
 
     it('should return true if a hashedReplacementURL exists', () => {
-      const hasReplacementURL = ap.hasHashedReplacementURL('/foo/test.css', fileObj);
+      const hasReplacementURL = ap.hasHashedReplacementURL(
+        '/foo/test.css',
+        fileObj
+      );
       expect(hasReplacementURL).toBe(true);
     });
 
     it('can handle a query string', () => {
-      const hasReplacementURL = ap.hasHashedReplacementURL('/foo/test.css?foo=1', fileObj);
+      const hasReplacementURL = ap.hasHashedReplacementURL(
+        '/foo/test.css?foo=1',
+        fileObj
+      );
       expect(hasReplacementURL).toBe(true);
     });
 
     it('should return false if a hashedReplacementURL does not exist', () => {
-      const hasReplacementURL = ap.hasHashedReplacementURL('/foo/test2.css', fileObj);
+      const hasReplacementURL = ap.hasHashedReplacementURL(
+        '/foo/test2.css',
+        fileObj
+      );
       expect(hasReplacementURL).toBe(false);
     });
   });
@@ -120,22 +135,22 @@ describe('Asset Pipeline functions', () => {
         hashedPath: 'foo/test.7ff8923a.css',
       },
       'foo/test2.css': {
-        hashedPath: null
+        hashedPath: null,
       },
     };
 
     it('should provide a hashed path based on the input', () => {
-      const replacement = ap.getReplacementURL('/foo/test.css', fileObj)
+      const replacement = ap.getReplacementURL('/foo/test.css', fileObj);
       expect(replacement).toBe('/foo/test.7ff8923a.css');
     });
 
     it('should provide a hashed path based on the input', () => {
-      const replacement = ap.getReplacementURL('/foo/test.css?foo=1', fileObj)
+      const replacement = ap.getReplacementURL('/foo/test.css?foo=1', fileObj);
       expect(replacement).toBe('/foo/test.7ff8923a.css?foo=1');
     });
 
     it('should provide the same output as the input if not hashed', () => {
-      const replacement = ap.getReplacementURL('/foo/test2.css', fileObj)
+      const replacement = ap.getReplacementURL('/foo/test2.css', fileObj);
       expect(replacement).toBe('/foo/test2.css');
     });
   });
@@ -146,37 +161,37 @@ describe('Asset Pipeline functions', () => {
     });
 
     it('should update the assetMap for files matching a certain pattern', async () => {
-      await ap.updateHashMap(/\.css$/)
+      await ap.updateHashMap(/\.css$/);
       expect(ap.assetMap).toEqual({
-        "assets/file1.css": {
-          "hash": "072a023e300cf4a837e64807f5cbacf432b885f34738a466b7ee6f1e9a9476b3",
-          "hashedPath": "assets/file1.072a023e.css",
-          "shortHash": "072a023e",
-          "written": false,
+        'assets/file1.css': {
+          hash:
+            '072a023e300cf4a837e64807f5cbacf432b885f34738a466b7ee6f1e9a9476b3',
+          hashedPath: 'assets/file1.072a023e.css',
+          shortHash: '072a023e',
+          written: false,
         },
-        "assets/file2.js":  {
-          "hashedPath": null,
+        'assets/file2.js': {
+          hashedPath: null,
         },
-        "assets/octoamo-sm.png": {
-          "hashedPath": null,
+        'assets/octoamo-sm.png': {
+          hashedPath: null,
         },
-        "index.html": {
-          "hashedPath": null,
+        'index.html': {
+          hashedPath: null,
         },
-        "robots.txt": {
-          "hashedPath": null,
+        'robots.txt': {
+          hashedPath: null,
         },
       });
     });
 
     it('should call the callback when provided', async () => {
       const myAsyncMock = jest.fn();
-      await ap.updateHashMap(/\.css$/, myAsyncMock)
+      await ap.updateHashMap(/\.css$/, myAsyncMock);
       expect(myAsyncMock.mock.calls.length).toBe(1);
     });
   });
 });
-
 
 describe('Asset Pipeline cacheBusting', () => {
   describe('cachebustAssets', () => {
@@ -187,32 +202,46 @@ describe('Asset Pipeline cacheBusting', () => {
     });
 
     it('should rewrite a binary file to dest', async () => {
-      const exists = await fs.stat(path.join(dest, ap.assetMap['assets/octoamo-sm.png'].hashedPath)).catch((e) => false);
+      const exists = await fs
+        .stat(path.join(dest, ap.assetMap['assets/octoamo-sm.png'].hashedPath))
+        .catch((e) => false);
       expect(exists).not.toBe(false);
     });
 
     it('should rewrite a js file to dest', async () => {
-      const exists = await fs.stat(path.join(dest, ap.assetMap['assets/file2.js'].hashedPath)).catch((e) => false);
+      const exists = await fs
+        .stat(path.join(dest, ap.assetMap['assets/file2.js'].hashedPath))
+        .catch((e) => false);
       expect(exists).not.toBe(false);
     });
 
     it('should have rewritten asset refs in js', async () => {
-      const jsFile = await fs.readFile(path.join(dest, ap.assetMap['assets/file2.js'].hashedPath), 'utf8');
+      const jsFile = await fs.readFile(
+        path.join(dest, ap.assetMap['assets/file2.js'].hashedPath),
+        'utf8'
+      );
       expect(jsFile).toContain(ap.assetMap['assets/octoamo-sm.png'].hashedPath);
     });
 
     it('should rewrite a css file to dest', async () => {
-      const exists = await fs.stat(path.join(dest, ap.assetMap['assets/file1.css'].hashedPath)).catch((e) => false);
+      const exists = await fs
+        .stat(path.join(dest, ap.assetMap['assets/file1.css'].hashedPath))
+        .catch((e) => false);
       expect(exists).not.toBe(false);
     });
 
     it('should have rewritten asset refs in css', async () => {
-      const css = await fs.readFile(path.join(dest, ap.assetMap['assets/file1.css'].hashedPath), 'utf8');
+      const css = await fs.readFile(
+        path.join(dest, ap.assetMap['assets/file1.css'].hashedPath),
+        'utf8'
+      );
       expect(css).toContain(ap.assetMap['assets/octoamo-sm.png'].hashedPath);
     });
 
     it('should rewrite an html file to dest', async () => {
-      const exists = await fs.stat(path.join(dest, 'index.html')).catch((e) => false);
+      const exists = await fs
+        .stat(path.join(dest, 'index.html'))
+        .catch((e) => false);
       expect(exists).not.toBe(false);
     });
 
@@ -224,7 +253,9 @@ describe('Asset Pipeline cacheBusting', () => {
     });
 
     it('should rewrite an robots.txt file to dest', async () => {
-      const exists = await fs.stat(path.join(dest, 'robots.txt')).catch((e) => false);
+      const exists = await fs
+        .stat(path.join(dest, 'robots.txt'))
+        .catch((e) => false);
       expect(exists).not.toBe(false);
     });
   });
