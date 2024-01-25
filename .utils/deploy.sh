efe13366477b8a879d35e40a7bd3f8f0d6cc8876
Brandon Patterson <bpatterson@mozilla.com>
2024-01-25 17:58:55 -0500

    update script to work with gcp

--- .utils/deploy.sh
+++ .utils/deploy.sh
@@ -7,6 +7,17 @@
 
 set -ex
 
+# if [ ! -d "dist" ]; then
+#     echo "Can't find /dist/ directory. Are you running from the correct"\
+#          "root directory?"
+#     exit 1
+# fi
+
+# if [ -z "$EXTENSION_WORKSHOP_BUCKET" ]; then
+#     echo "The S3 bucket is not set. Failing."
+#     exit 1
+# fi
+
 if [ ! -d "dist" ]; then
     echo "Can't find /dist/ directory. Are you running from the correct"\
          "root directory?"
@@ -14,9 +25,10 @@ if [ ! -d "dist" ]; then
 fi
 
 if [ -z "$EXTENSION_WORKSHOP_BUCKET" ]; then
-    echo "The S3 bucket is not set. Failing."
+    echo "The GCS bucket is not set. Failing."
     exit 1
 fi
+
 
 # The basic strategy is to sync all the files that need special attention
 # first, and then sync everything else which will get defaults
@@ -56,59 +68,132 @@ ACAO="\"Access-Control-Allow-Origin\": \"*\""
 # build version.json if it isn't provided
 [ -e version.json ] || $(dirname $0)/build-version-json.sh
 
+# if [ -e version.json ]; then
+#     mv version.json dist/__version__
+#     # __version__ JSON; short cache
+#     aws s3 cp \
+#       --cache-control "max-age=${TEN_MINUTES}" \
+#       --content-type "application/json" \
+#       --metadata "{${ACAO}, ${CSPSTATIC}, ${HSTS}, ${TYPE}, ${XSS}, ${XFRAME}, ${REFERRER}}" \
+#       --metadata-directive "REPLACE" \
+#       --acl "public-read" \
+#       dist/__version__ s3://${EXTENSION_WORKSHOP_BUCKET}/__version__
+# fi
 if [ -e version.json ]; then
     mv version.json dist/__version__
     # __version__ JSON; short cache
-    aws s3 cp \
-      --cache-control "max-age=${TEN_MINUTES}" \
-      --content-type "application/json" \
-      --metadata "{${ACAO}, ${CSPSTATIC}, ${HSTS}, ${TYPE}, ${XSS}, ${XFRAME}, ${REFERRER}}" \
-      --metadata-directive "REPLACE" \
-      --acl "public-read" \
-      dist/__version__ s3://${EXTENSION_WORKSHOP_BUCKET}/__version__
+    gsutil -h "Cache-Control:max-age=${TEN_MINUTES}" \
+      -h "Content-Type:application/json" \
+      -h "Metadata:${ACAO},${CSPSTATIC},${HSTS},${TYPE},${XSS},${XFRAME},${REFERRER}" \
+      -h "x-goog-metadata-directive:REPLACE" \
+      -a public-read cp dist/__version__ gs://$EXTENSION_WORKSHOP_BUCKET/__version__
 fi
 
+upload_file() {
+  local file=$1
+  local content_type=$2
+  local cache_control=$3
+  local metadata=$4
+
+  gsutil -h "Cache-Control:${cache_control}" \
+    -h "Content-Type:${content_type}" \
+    ...
+}
+
+HTML_METADATA="${ACAO},${CSP},${HSTS},${TYPE},${XSS},${XFRAME},${REFERRER}"
+JS_METADATA="${ACAO},${CSPSTATIC},..."
+SVG_METADATA="${CSPSTATIC},${HSTS},..."
+TEN_MINUTES="max-age=600"
+ONE_YEAR="max-age=31536000, immutable"
+
+find dist -type f -not -path 'dist/__version__/*' | while read -r fn; do
+  if [[ $fn == *.html ]]; then
+    upload_file "$fn" "text/html" "${TEN_MINUTES}" "${HTML_METADATA}"
+  elif [[ $fn == *.js ]]; then
+    upload_file "$fn" "application/javascript" "${TEN_MINUTES}" "${JS_METADATA}"
+  elif [[ $fn == *.svg ]]; then
+    upload_file "$fn" "image/svg+xml" "${ONE_YEAR}" "${SVG_METADATA}"
+ 
+  else upload_file "$fn"  <content-type>  "${}" "${}"
+ if 
+
+done
+
+
+
+
+
+
+
+# for fn in $(find dist -name '*.html' -not -path 'dist/__version__'); do
+#   # HTML; short cache
+#   gsutil -h "Cache-Control:max-age=${TEN_MINUTES}" \
+#     -h "Content-Type:text/html" \
+#     -h "Metadata:${ACAO},${CSP},${HSTS},${TYPE},${XSS},${XFRAME},${REFERRER}" \
+#     -h "x-goog-metadata-directive:REPLACE" \
+#     -a public-read cp $fn gs://$EXTENSION_WORKSHOP_BUCKET/$fn
+# done
+
+# for fn in $(find dist -name '*.js' -not -path 'dist/__version__'); do
+#   # JS; short cache
+#   gsutil -h "Cache-Control:max-age=${TEN_MINUTES}" \
+#     -h "Content-Type:application/javascript" \
+#     -h "Metadata:${ACAO},${CSPSTATIC},${HSTS},${TYPE},${XSS},${XFRAME},${REFERRER}" \
+#     -h "x-goog-metadata-directive:REPLACE" \
+#     -a public-read cp $fn gs://$EXTENSION_WORKSHOP_BUCKET/$fn
+# done
+
+# for fn in $(find dist -name '*.svg' -not -path 'dist/__version__'); do
+#   # SVG; cache forever, assign correct content-type
+#   gsutil -h "Cache-Control:max-age=${ONE_YEAR}, immutable" \
+#     -h "Content-Type:image/svg+xml" \
+#     -h "Metadata:${CSPSTATIC},${HSTS},${TYPE},${XSS},${XFRAME},${REFERRER}" \
+#     -h "x-goog-metadata-directive:REPLACE" \
+#     -a public-read cp $fn gs://$EXTENSION_WORKSHOP_BUCKET/$fn
+# done
+
+
 # HTML; short cache
-aws s3 sync \
-  --cache-control "max-age=${TEN_MINUTES}" \
-  --content-type "text/html" \
-  --exclude "*" \
-  --include "*.html" \
-  --metadata "{${CSP}, ${HSTS}, ${TYPE}, ${XSS}, ${XFRAME}, ${REFERRER}}" \
-  --metadata-directive "REPLACE" \
-  --acl "public-read" \
-  dist/ s3://${EXTENSION_WORKSHOP_BUCKET}/
+# aws s3 sync \
+#   --cache-control "max-age=${TEN_MINUTES}" \
+#   --content-type "text/html" \
+#   --exclude "*" \
+#   --include "*.html" \
+#   --metadata "{${CSP}, ${HSTS}, ${TYPE}, ${XSS}, ${XFRAME}, ${REFERRER}}" \
+#   --metadata-directive "REPLACE" \
+#   --acl "public-read" \
+#   dist/ s3://${EXTENSION_WORKSHOP_BUCKET}/
 
 # JSON; short cache
-aws s3 sync \
-  --cache-control "max-age=${TEN_MINUTES}" \
-  --content-type "application/json" \
-  --exclude "*" \
-  --include "*.json" \
-  --metadata "{${ACAO}, ${CSPSTATIC}, ${HSTS}, ${TYPE}, ${XSS}, ${XFRAME}, ${REFERRER}}" \
-  --metadata-directive "REPLACE" \
-  --acl "public-read" \
-  dist/ s3://${EXTENSION_WORKSHOP_BUCKET}/
+# aws s3 sync \
+#   --cache-control "max-age=${TEN_MINUTES}" \
+#   --content-type "application/json" \
+#   --exclude "*" \
+#   --include "*.json" \
+#   --metadata "{${ACAO}, ${CSPSTATIC}, ${HSTS}, ${TYPE}, ${XSS}, ${XFRAME}, ${REFERRER}}" \
+#   --metadata-directive "REPLACE" \
+#   --acl "public-read" \
+#   dist/ s3://${EXTENSION_WORKSHOP_BUCKET}/
 
 # SVG; cache forever, assign correct content-type
-aws s3 sync \
-  --cache-control "max-age=${ONE_YEAR}, immutable" \
-  --content-type "image/svg+xml" \
-  --exclude "*" \
-  --include "*.svg" \
-  --metadata "{${CSPSTATIC}, ${HSTS}, ${TYPE}, ${XSS}, ${XFRAME}, ${REFERRER}}" \
-  --metadata-directive "REPLACE" \
-  --acl "public-read" \
-  dist/ s3://${EXTENSION_WORKSHOP_BUCKET}/
+# aws s3 sync \
+#   --cache-control "max-age=${ONE_YEAR}, immutable" \
+#   --content-type "image/svg+xml" \
+#   --exclude "*" \
+#   --include "*.svg" \
+#   --metadata "{${CSPSTATIC}, ${HSTS}, ${TYPE}, ${XSS}, ${XFRAME}, ${REFERRER}}" \
+#   --metadata-directive "REPLACE" \
+#   --acl "public-read" \
+#   dist/ s3://${EXTENSION_WORKSHOP_BUCKET}/
 
 # Everything else; cache forever, because it has hashes in the filenames
-aws s3 sync \
-  --delete \
-  --cache-control "max-age=${ONE_YEAR}, immutable" \
-  --metadata "{${CSPSTATIC}, ${HSTS}, ${TYPE}, ${XSS}, ${XFRAME}, ${REFERRER}}" \
-  --metadata-directive "REPLACE" \
-  --acl "public-read" \
-  dist/ s3://${EXTENSION_WORKSHOP_BUCKET}/
+# aws s3 sync \
+#   --delete \
+#   --cache-control "max-age=${ONE_YEAR}, immutable" \
+#   --metadata "{${CSPSTATIC}, ${HSTS}, ${TYPE}, ${XSS}, ${XFRAME}, ${REFERRER}}" \
+#   --metadata-directive "REPLACE" \
+#   --acl "public-read" \
+#   dist/ s3://${EXTENSION_WORKSHOP_BUCKET}/
 
 # HTML - `path/index.html` to `path` resources; short cache
 for fn in $(find dist -name 'index.html' -not -path 'dist/index.html'); do
