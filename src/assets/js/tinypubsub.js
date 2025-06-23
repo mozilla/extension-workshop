724d0629f135ee6351b75500ec633a674ff5c048
William Durand <will+git@drnd.me>
2025-06-23 20:56:40 +0200

    run prettier

--- src/assets/js/tinypubsub.js
+++ src/assets/js/tinypubsub.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2011 "Cowboy" Ben Alman; Licensed MIT, GPL */
 (function (a) {
   var b = a({});
-  (a.subscribe = function () {
+  ((a.subscribe = function () {
     b.on.apply(b, arguments);
   }),
     (a.unsubscribe = function () {
@@ -11,5 +11,5 @@
     }),
     (a.publish = function () {
       b.trigger.apply(b, arguments);
-    });
+    }));
 })(jQuery);
